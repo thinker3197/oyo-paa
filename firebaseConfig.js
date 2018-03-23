8cde193b2ff5344b540e562ae4da4590fcd0df28
thinker3197 <ashishgupta.3197@gmail.com>
2018-03-23 08:58:03 +0530

    Add firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyALjqmgkTQa6zaKHx8qCR-FWM-whOpeclg",
+  authDomain: "oyo-paa.firebaseapp.com",
+  databaseURL: "https://oyo-paa.firebaseio.com",
+  projectId: "oyo-paa",
+  storageBucket: "oyo-paa.appspot.com",
+  messagingSenderId: "434104200190"
+};
+
+module.exports = config;
